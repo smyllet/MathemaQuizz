abdffc6e015555a1b79de3bbe9ded09a9a5d0abc
Smyllet <smyllet.gamer@gmail.com>
2021-10-21 09:52:27 +0200

    Itération 2

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,17 @@
 public class Game {
     public void startGame() {
-        AdditionQuestion question = new AdditionQuestion();
-        System.out.println("Réalisez l'opération suivante : " + question.getQuestion());
-        int userAnswer = Utils.inputInt(0, 198, "> ");
-        if(userAnswer == question.getAnswer()) System.out.println("Félicitation !");
-        else System.out.println("Mauvaise réponse, la réponse était : " + question.getAnswer());
+        int score = 0;
+        System.out.println("Veuillez réaliser les 10 opérations suivantes : ");
+        for(int i = 1; i<=10; i++) {
+            AdditionQuestion question = new AdditionQuestion();
+            System.out.println("Opération n°" + i + " : " + question.getQuestion());
+            int userAnswer = Utils.inputInt(0, 198, "> ");
+            if(userAnswer == question.getAnswer()) {
+                System.out.println("Félicitation !");
+                score++;
+            }
+            else System.out.println("Mauvaise réponse, la réponse était : " + question.getAnswer());
+        }
+        System.out.println("Fin de la partie, votre score est de " + score + "/10");
     }
 }
